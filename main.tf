b5a770ebd04685188dd6757bafbe27e4314b0730
nareshungarala396 <nareshungarala220@gmail.com>
2025-11-06 13:10:51 +0530

    roboshop

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-    source = "../terraform-aws-vpc"
+    source = "https://github.com/nareshungarala396/terraform-aws-vpc.git?ref=main"
     cidr_block = var.vpc_cidr
     project_name = var.project_name
     environment = var.environment

97d0501268b9ae2c62fb00a4d3b8f921d8b3cdf2
nareshungarala396 <nareshungarala220@gmail.com>
2025-11-09 11:17:47 +0530

    roboshop

--- variables.tf
+++ variables.tf
@@ -23,7 +23,7 @@ variable "public_subnet_cidrs" {
     default = ["10.0.1.0/24","10.0.2.0/24"]
 }
 
-varaible "private_subnet_cidrs" {
+variable "private_subnet_cidrs" {
     type = list
     default = ["10.0.11.0/24","10.0.12.0/24"]
 }
